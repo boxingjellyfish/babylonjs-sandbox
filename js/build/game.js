b4e983619550e691009ee21cd7780595c48d3781
Boxing <dev@boxingjellyfish.com>
2018-03-21 12:44:15 -0300

    change scene fixed

--- js/build/game.js
+++ js/build/game.js
@@ -6,9 +6,6 @@ var Game = /** @class */ (function () {
         // Create canvas and engine
         this.canvas = document.getElementById(canvasElement);
         this.engine = new BABYLON.Engine(this.canvas, true);
-        // Initi scene collection
-        this.scenes = [];
-        this.currentSceneIndex = 0;
         // Listen for browser/canvas resize events
         window.addEventListener("resize", function () {
             _this.engine.resize();
@@ -17,84 +14,132 @@ var Game = /** @class */ (function () {
         this.canvas.oncontextmenu = function (e) {
             e.preventDefault();
         };
+        this.scene = this.createScene1();
     }
-    Game.prototype.createScene = function () {
+    Game.prototype.createScene1 = function () {
+        var _this = this;
         // create a basic BJS Scene object
         var scene = new BABYLON.Scene(this.engine);
         scene.clearColor = new BABYLON.Color4(0, 0, 0, 1);
         // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
-        this.camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
+        var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
         // target the camera to scene origin
-        this.camera.setTarget(BABYLON.Vector3.Zero());
+        camera.setTarget(BABYLON.Vector3.Zero());
         // attach the camera to the canvas
-        this.camera.attachControl(this.canvas, false);
+        camera.attachControl(this.canvas, false);
         // create a basic light, aiming 0,1,0 - meaning, to the sky
-        this.light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
+        var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+        //this.light = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(1, 10, 1), scene);
         // create a built-in "sphere" shape; with 16 segments and diameter of 2
-        var sphere = BABYLON.MeshBuilder.CreateSphere('sphere', { segments: 16, diameter: 2 }, scene);
+        var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { segments: 16, diameter: 2 }, scene);
         // move the sphere upward 1/2 of its height
         sphere.position.y = 1;
         // create a built-in "ground" shape
-        var ground = BABYLON.MeshBuilder.CreateGround('ground', { width: 6, height: 6, subdivisions: 2 }, scene);
-        // Show debug layer
-        scene.debugLayer.show();
+        var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6, subdivisions: 2 }, scene);
         // GUI
         var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
         var button1 = BABYLON.GUI.Button.CreateSimpleButton("but1", "Change Scene");
-        button1.width = 0.2;
+        button1.width = "220px";
         button1.height = "40px";
         button1.color = "white";
-        button1.cornerRadius = 20;
         button1.background = "blue";
+        button1.top = "20px";
         button1.left = "20px";
-        button1.onPointerUpObservable.add(this.changeScene);
+        button1.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+        button1.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
+        button1.onPointerUpObservable.add(function () {
+            _this.scene.dispose();
+            _this.scene = _this.createScene2();
+        });
         advancedTexture.addControl(button1);
-        // Add to scene collection
-        this.scenes.push(scene);
+        var button2 = BABYLON.GUI.Button.CreateSimpleButton("but2", "Debug");
+        button2.width = "220px";
+        button2.height = "40px";
+        button2.color = "white";
+        button2.background = "orange";
+        button2.top = "-20px";
+        button2.left = "20px";
+        button2.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+        button2.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+        button2.onPointerUpObservable.add(function () {
+            if (scene.debugLayer.isVisible()) {
+                scene.debugLayer.hide();
+            }
+            else {
+                scene.debugLayer.show();
+            }
+        });
+        advancedTexture.addControl(button2);
+        return scene;
     };
     Game.prototype.createScene2 = function () {
+        var _this = this;
         // create a basic BJS Scene object
         var scene = new BABYLON.Scene(this.engine);
         scene.clearColor = new BABYLON.Color4(0.5, 0.5, 0.5, 1);
         // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
-        this.camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
+        var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -20), scene);
         // target the camera to scene origin
-        this.camera.setTarget(BABYLON.Vector3.Zero());
+        camera.setTarget(BABYLON.Vector3.Zero());
         // attach the camera to the canvas
-        this.camera.attachControl(this.canvas, false);
+        camera.attachControl(this.canvas, false);
+        // create a basic light, aiming 0,1,0 - meaning, to the sky
+        var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 1), scene);
+        //this.light = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(5, 10, 2), scene);
+        // create a built-in "sphere" shape; with 16 segments and diameter of 2
+        var sphere = BABYLON.MeshBuilder.CreateBox("box", { size: 2 }, scene);
+        // move the sphere upward 1/2 of its height
+        sphere.position.y = 1;
+        // create a built-in "ground" shape
+        var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6, subdivisions: 2 }, scene);
         // GUI
         var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
         var button1 = BABYLON.GUI.Button.CreateSimpleButton("but1", "Change Scene");
-        button1.width = 0.2;
+        button1.width = "220px";
         button1.height = "40px";
         button1.color = "white";
-        button1.cornerRadius = 20;
         button1.background = "green";
-        button1.onPointerUpObservable.add(this.changeScene);
+        button1.top = "20px";
+        button1.left = "-20px";
+        button1.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
+        button1.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
+        button1.onPointerUpObservable.add(function () {
+            _this.scene.dispose();
+            _this.scene = _this.createScene1();
+        });
         advancedTexture.addControl(button1);
-        // Add to scene collection
-        this.scenes.push(scene);
-    };
-    Game.prototype.changeScene = function () {
-        this.currentSceneIndex = this.currentSceneIndex == 0 ? 1 : 0;
-        console.log("Scene changed to: " + this.currentSceneIndex);
+        var button2 = BABYLON.GUI.Button.CreateSimpleButton("but2", "Debug");
+        button2.width = "220px";
+        button2.height = "40px";
+        button2.color = "white";
+        button2.background = "orange";
+        button2.top = "-20px";
+        button2.left = "20px";
+        button2.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+        button2.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+        button2.onPointerUpObservable.add(function () {
+            if (scene.debugLayer.isVisible()) {
+                scene.debugLayer.hide();
+            }
+            else {
+                scene.debugLayer.show();
+            }
+        });
+        advancedTexture.addControl(button2);
+        return scene;
     };
     Game.prototype.run = function () {
         var _this = this;
         // run the render loop
         this.engine.runRenderLoop(function () {
-            _this.scenes[_this.currentSceneIndex].render();
+            _this.scene.render();
         });
     };
     return Game;
 }());
-// Create the game using the 'renderCanvas'
-var game = new Game('renderCanvas');
+// Create the game using the "renderCanvas"
+var game = new Game("renderCanvas");
 console.log("Game object created");
-// Create both scenes
-game.createScene();
-game.createScene2();
-console.log("Scene objects created");
 // start animation
 game.run();
 console.log("Run started");
--- ts/game.ts
+++ ts/game.ts
@@ -4,19 +4,12 @@
 class Game {
     private canvas: HTMLCanvasElement;
     private engine: BABYLON.Engine;
-    private scenes: BABYLON.Scene[];
-    private currentSceneIndex: number;
-    private camera: BABYLON.FreeCamera;
-    private light: BABYLON.Light;
+    private scene: BABYLON.Scene;
 
     constructor(canvasElement: string) {
         // Create canvas and engine
         this.canvas = document.getElementById(canvasElement) as HTMLCanvasElement;
         this.engine = new BABYLON.Engine(this.canvas, true);
-
-        // Initi scene collection
-        this.scenes = [];
-        this.currentSceneIndex = 0;
 
         // Listen for browser/canvas resize events
         window.addEventListener("resize", () => {
@@ -27,107 +20,153 @@ class Game {
         this.canvas.oncontextmenu = function (e) {
             e.preventDefault();
         };
+
+        this.scene = this.createScene1();
     }
 
-    createScene(): void {
+    createScene1(): BABYLON.Scene {
         // create a basic BJS Scene object
         let scene = new BABYLON.Scene(this.engine);
         scene.clearColor = new BABYLON.Color4(0, 0, 0, 1);
 
         // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
-        this.camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
+        let camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
 
         // target the camera to scene origin
-        this.camera.setTarget(BABYLON.Vector3.Zero());
+        camera.setTarget(BABYLON.Vector3.Zero());
 
         // attach the camera to the canvas
-        this.camera.attachControl(this.canvas, false);
+        camera.attachControl(this.canvas, false);
 
         // create a basic light, aiming 0,1,0 - meaning, to the sky
-        this.light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
+        let light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+        //this.light = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(1, 10, 1), scene);
 
         // create a built-in "sphere" shape; with 16 segments and diameter of 2
-        let sphere = BABYLON.MeshBuilder.CreateSphere('sphere',
-            { segments: 16, diameter: 2 }, scene);
+        let sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { segments: 16, diameter: 2 }, scene);
 
         // move the sphere upward 1/2 of its height
         sphere.position.y = 1;
 
         // create a built-in "ground" shape
-        let ground = BABYLON.MeshBuilder.CreateGround('ground',
-            { width: 6, height: 6, subdivisions: 2 }, scene);
-
-        // Show debug layer
-        scene.debugLayer.show();
+        let ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6, subdivisions: 2 }, scene);
 
         // GUI
         let advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
 
         let button1 = BABYLON.GUI.Button.CreateSimpleButton("but1", "Change Scene");
-        button1.width = 0.2;
+        button1.width = "220px";
         button1.height = "40px";
         button1.color = "white";
-        button1.cornerRadius = 20;
         button1.background = "blue";
-        button1.left = "20px"
-        button1.onPointerUpObservable.add(this.changeScene);
+        button1.top = "20px";
+        button1.left = "20px";
+        button1.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+        button1.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
+        button1.onPointerUpObservable.add(() => {
+            this.scene.dispose();
+            this.scene = this.createScene2();
+        });
         advancedTexture.addControl(button1);
 
-        // Add to scene collection
-        this.scenes.push(scene)
+        let button2 = BABYLON.GUI.Button.CreateSimpleButton("but2", "Debug");
+        button2.width = "220px";
+        button2.height = "40px";
+        button2.color = "white";
+        button2.background = "orange";
+        button2.top = "-20px";
+        button2.left = "20px";
+        button2.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+        button2.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+        button2.onPointerUpObservable.add(() => {
+            if (scene.debugLayer.isVisible()) {
+                scene.debugLayer.hide();
+            } else {
+                scene.debugLayer.show();
+            }
+        });
+        advancedTexture.addControl(button2);
+
+        return scene;
     }
 
-    createScene2(): void {
+    createScene2(): BABYLON.Scene {
         // create a basic BJS Scene object
         let scene = new BABYLON.Scene(this.engine);
         scene.clearColor = new BABYLON.Color4(0.5, 0.5, 0.5, 1);
 
         // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
-        this.camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
+        let camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -20), scene);
 
         // target the camera to scene origin
-        this.camera.setTarget(BABYLON.Vector3.Zero());
+        camera.setTarget(BABYLON.Vector3.Zero());
 
         // attach the camera to the canvas
-        this.camera.attachControl(this.canvas, false);
+        camera.attachControl(this.canvas, false);
+
+        // create a basic light, aiming 0,1,0 - meaning, to the sky
+        let light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 1), scene);
+        //this.light = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(5, 10, 2), scene);
+
+        // create a built-in "sphere" shape; with 16 segments and diameter of 2
+        let sphere = BABYLON.MeshBuilder.CreateBox("box", { size: 2 }, scene);
+
+        // move the sphere upward 1/2 of its height
+        sphere.position.y = 1;
+
+        // create a built-in "ground" shape
+        let ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6, subdivisions: 2 }, scene);
 
         // GUI
         let advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
 
         let button1 = BABYLON.GUI.Button.CreateSimpleButton("but1", "Change Scene");
-        button1.width = 0.2;
+        button1.width = "220px";
         button1.height = "40px";
         button1.color = "white";
-        button1.cornerRadius = 20;
         button1.background = "green";
-        button1.onPointerUpObservable.add(this.changeScene);
+        button1.top = "20px";
+        button1.left = "-20px";
+        button1.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
+        button1.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
+        button1.onPointerUpObservable.add(() => {
+            this.scene.dispose();
+            this.scene = this.createScene1();
+        });
         advancedTexture.addControl(button1);
 
-        // Add to scene collection
-        this.scenes.push(scene)
-    }
+        let button2 = BABYLON.GUI.Button.CreateSimpleButton("but2", "Debug");
+        button2.width = "220px";
+        button2.height = "40px";
+        button2.color = "white";
+        button2.background = "orange";
+        button2.top = "-20px";
+        button2.left = "20px";
+        button2.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+        button2.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+        button2.onPointerUpObservable.add(() => {
+            if (scene.debugLayer.isVisible()) {
+                scene.debugLayer.hide();
+            } else {
+                scene.debugLayer.show();
+            }
+        });
+        advancedTexture.addControl(button2);
 
-    changeScene(): void {
-        this.currentSceneIndex = this.currentSceneIndex == 0 ? 1 : 0;
-        console.log("Scene changed to: " + this.currentSceneIndex);
+        return scene;
     }
 
     run(): void {
         // run the render loop
         this.engine.runRenderLoop(() => {
-            this.scenes[this.currentSceneIndex].render();
+            this.scene.render();
         });
     }
 }
 
-// Create the game using the 'renderCanvas'
-let game = new Game('renderCanvas');
+// Create the game using the "renderCanvas"
+let game = new Game("renderCanvas");
 console.log("Game object created")
-
-// Create both scenes
-game.createScene();
-game.createScene2();
-console.log("Scene objects created")
 
 // start animation
 game.run();
